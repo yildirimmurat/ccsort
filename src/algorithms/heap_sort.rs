a19e02e9f817fbe014b289ee94a4ce447087ee0e
yildirimmurat <myildirim.89@gmail.com>
2024-12-17 01:55:52 +0100

    heap sort

--- src/algorithms/heap_sort.rs
+++ src/algorithms/heap_sort.rs
@@ -1,3 +1,17 @@
-pub(crate) fn sort(_p0: &mut [String]) {
-    todo!()
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+pub fn sort(arr: &mut [String]) {
+    let mut heap = BinaryHeap::new();
+
+    // wrapped in Reverse to sort lexicographically
+    for item in arr.iter() {
+        heap.push(Reverse(item.clone()));
+    }
+
+    let mut i = 0;
+    while let Some(Reverse(item)) = heap.pop() {
+        arr[i] = item;
+        i += 1;
+    }
 }
+
--- tests/integration.rs
+++ tests/integration.rs
@@ -62,6 +62,7 @@ AND";
 
     assert_eq!(expected, actual);
 }
+
 #[test]
 fn test_quick_sort() {
     let expected: &str = "\
@@ -74,6 +75,27 @@ AND";
     let output = std::process::Command::new("bash")
         .arg("-c")
         .arg("cargo run -- -u -q tests/words.txt | head -n5")
+        .output()
+    .expect("failed to execute process");
+
+    let binding: Cow<str> = String::from_utf8_lossy(&output.stdout);
+    let actual: &str = binding.trim(); // Remove BOM character at the start
+
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_heap_sort() {
+    let expected: &str = "\
+A\n\
+ACTUAL\n\
+AGREE\n\
+AGREEMENT\n\
+AND";
+
+    let output = std::process::Command::new("bash")
+        .arg("-c")
+        .arg("cargo run -- -u -h tests/words.txt | head -n5")
         .output()
     .expect("failed to execute process");
 
